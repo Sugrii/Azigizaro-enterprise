b17a50f2b3f0ddc2351c0dcde6cbc5a837cac4b3
Sugrii <124563039+Sugrii@users.noreply.github.com>
2026-08-06 02:37:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -231,7 +231,7 @@
     } else if (view === 'login') {
       document.getElementById('loginForm').style.display = 'flex';
       title.innerText = "🔐 Sign In";
-      subtitle.innerText = `Welcome back! ${username} . Enter credentials to access your store.`;
+      subtitle.innerText = `Welcome back! ${username} Enter credentials to access your store.`;
     } else if (view === 'subscribe') {
       document.getElementById('subscribeForm').style.display = 'flex';
       title.innerText = "🚀 Subscribe to Azigizaro Enterprise Shop Manager";
